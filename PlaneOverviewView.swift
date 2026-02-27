ee93dfcaba69079145d0ff324a69153eb977b03d
ArcCr8iveStudios <mnelson6196@gmail.com>
2026-02-26 21:20:25 -0800

    PlaneOverviewView

---
+++ PlaneOverviewView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct PlaneOverviewView: View {
+    var body: some View {
+        VStack {
+            Text("Plane Overview")
+                .font(.largeTitle)
+                .padding()
+            
+            Text("Your focus stats and upgrades will live here.")
+                .padding()
+        }
+    }
+}
+
