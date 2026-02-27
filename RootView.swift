911180117591d965ee34d5480b77b815d14e2ca1
ArcCr8iveStudios <mnelson6196@gmail.com>
2026-02-26 21:21:28 -0800

    RootView

---
+++ RootView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct RootView: View {
+    
+    @State private var missions: [Mission] = []
+    @State private var activeMission: Mission? = nil
+    
+    var body: some View {
+        NavigationStack {
+            HomeView(
+                missions: $missions,
+                activeMission: $activeMission
+            )
+        }
+    }
+}
